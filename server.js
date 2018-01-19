603fc9c2f6a947956f46d4ddf652b2ca9f2cede5
DESKTOP-V02D53O\champ <champtnpc@gmail.com>
2018-01-19 21:31:17 +0700

    first commit2

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
-
+const app = express()
 router.get('/', function (req, res) {
   res.send('Error, wrong token')
+})
+
+app.set('port', (process.env.PORT || 5000))
+app.get('/', function (req, res) {
+  res.send('<h1 align="center">Phureerak</h1>')
+})
+app.listen(app.get('port'), function () {
+  console.log('running on port', app.get('port'))
 })
 
 module.exports = router
